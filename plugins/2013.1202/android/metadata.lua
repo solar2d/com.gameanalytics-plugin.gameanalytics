3e943c2c849a571a926d5e5494a0465b289b6006
Vlad Shcherban <vlad@coronalabs.com>
2020-06-06 19:53:30 +0300

    Updating binaries

---
+++ plugins/2013.1202/android/metadata.lua
@@ -0,0 +1,18 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'so',
+		manifest = 
+		{
+			permissions = {},
+			usesPermissions =
+			{
+				"android.permission.INTERNET",
+				"android.permission.ACCESS_NETWORK_STATE",
+			},
+		},
+	},
+}
+
+return metadata
